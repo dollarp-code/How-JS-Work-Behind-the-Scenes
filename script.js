b7887264efa3efcdef8c4fb0c29cd0d56d535274
dollarp-code <husseindollarp@yahoo.com>
2024-03-11 14:32:30 +0100

    Understanding Hoisting in JavaScript

--- script.js
+++ script.js
@@ -33,3 +33,49 @@ function calcAge(birthyear) {
 
 const firstName = 'qudus';
 calcAge(1996);
+
+// Hoisting with variables
+
+console.log(me);
+// console.log(age);
+// console.log(job);
+
+var me = 'qudus'; // undefined
+const age = 29; // error
+let job = 'programmer'; // error
+
+// Hoisting with functions
+
+console.log(addDecl(2, 3)); // 5
+// console.log(addExpr(2, 3)); // Error
+// console.log(addArrow(2, 3)); // Error
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+// Example window object
+
+var x = 1; // will create a property on the window object
+let y = 2; // will not create a property on the window object
+const z = 3; // will not create an object on the window property
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(y === window.y); // false
