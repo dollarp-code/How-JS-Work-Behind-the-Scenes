3f304b3b5a74e4ee1ad8c62889d1556f28b4d5b7
dollarp-code <husseindollarp@yahoo.com>
2024-03-11 11:58:07 +0100

    Understanding Scoping in JavaScript

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function calcAge(birthyear) {
+  const age = 2024 - birthyear;
+  //
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthyear}`;
+    console.log(output);
+
+    if (birthyear >= 1981 && birthyear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'steven';
+
+      // Reassigning outer scope's variable
+      output = 'NEW OUTPUT';
+
+      const str = `oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'qudus';
+calcAge(1996);
